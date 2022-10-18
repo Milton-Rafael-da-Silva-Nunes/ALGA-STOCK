36857e8e873ed71a313773cc19b3e7cca6cf2a55
rafael_nunes <rafa.nuns17@gmail.com>
2022-10-18 17:23:43 -0300

    Criei TesteComponente e removi o App do diretorio padrao

---
+++ src/componentes/TesteComponente/TesteComponente.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './TesteComponente.css';
+
+const TesteComponente = () => (
+<div className="TesteComponente">
+    Teste Componente!
+    </div>
+)
+
+export default TesteComponente

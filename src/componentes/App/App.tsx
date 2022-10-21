f931ed85ee5636aee4abeb75ebd129de609fb212
rafael_nunes <rafa.nuns17@gmail.com>
2022-10-21 12:31:24 -0300

    Usando (useState) na aplicacao

--- src/componentes/App/App.tsx
+++ src/componentes/App/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <TesteComponente />
+        <TesteComponente name="Milton"/>
         <ClassComponente name='Mundo!!!'/>
 
         <a

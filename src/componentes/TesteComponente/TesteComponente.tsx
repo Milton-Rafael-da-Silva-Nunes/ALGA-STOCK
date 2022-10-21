434af31da0d2799c623cbf232e2f2c96d70c662b
rafael_nunes <rafa.nuns17@gmail.com>
2022-10-21 13:59:41 -0300

    Removendo ClassComponente do App e add useEffect

--- src/componentes/TesteComponente/TesteComponente.tsx
+++ src/componentes/TesteComponente/TesteComponente.tsx
@@ -1,9 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './TesteComponente.css';
 
 function TesteComponente (props: {name: string}) {
 
     const [ age, setAge ] = useState(21)
+
+    useEffect(() => {
+        console.log('Componente Criado')
+    }, [])
+
+    useEffect(() => {
+        console.log('Age foi atualizado para: ' + age)
+    }, [age])
 
     return <div className="TesteComponente">
       Olá, {props.name}, { age }

ae1906290d2befc9134f0f809b1ace238deaf1b0
rafael_nunes <rafa.nuns17@gmail.com>
2022-10-25 14:56:30 -0300

    Adicionei o componente de Botao

--- src/componentes/App/App.tsx
+++ src/componentes/App/App.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
-import Header from '../Header';
 import './App.css';
+import Header from '../Header';
+import Button from '../Button';
+
+function TestComponent() {
+  return <img width="16" src="https://png.pngtree.com/element_our/20190529/ourlarge/pngtree-archaeological-research-magnifying-glass-illustration-image_1213916.jpg" 
+  alt="Lupa de pesquisa" 
+  />
+}
 
 function App() {
   return (
     <div className="App">
       <Header title={"AlgaStock"} />
-      App
+
+      <div className='Conteiner'>
+        <Button
+         onClick={() => window.alert('Clicouuu!!!')}
+         appendIcon={<TestComponent />} 
+         >
+          Alert
+        </Button>
+      </div>
     </div>
   );
 }
